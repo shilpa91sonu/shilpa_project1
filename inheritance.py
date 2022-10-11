fb357e3f70c86234243a6d05f456053ed62f6c40
shilpa91sonu <shilpa.y91@gmail.com>
2022-10-11 16:56:52 +0530

    coping file is taking place

---
+++ inheritance.py
@@ -0,0 +1,10 @@
+class student():
+	def student_name(self,name):
+		self.name=name
+		print"the student name is " + name
+	def student_college(self,college):
+		self.college=college
+		print"the student college " + college
+o1=student()
+o1.student_name("sonu")
+o1.student_college("msrit")
